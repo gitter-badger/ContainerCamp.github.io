84959b850f64720160a763a13db616d66b649ef5
Daniel Pyrathon <pirosb3@gmail.com>
2014-05-30 15:43:54 +0200

    added compatibility

--- _plugins/ordered_talks.rb
+++ _plugins/ordered_talks.rb
@@ -1,7 +1,9 @@
+require 'pry'
+
 module OrderedTalks
   class Generator < Jekyll::Generator
     def generate(site)
-      site.data['ordered_talks'] = site.collections["talks"].docs.sort_by do |t|
+      site.data['ordered_talks'] = site.posts.sort_by do |t|
           t.data["from"].sub(':', '').to_i
       end
     end
