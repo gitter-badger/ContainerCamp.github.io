20dc856b7e87c02c06f6e4d2ca9e8b7b7b68a5ba
Tim Ruffles <oi@truffles.me.uk>
2014-06-12 17:04:47 +0100

    update to avoid needing new paths

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,8 @@ echo ${DOCKER_CON_ENV:-STAGING}
 
 if [[ ${DOCKER_CON_ENV:-STAGING} == "PRODUCTION" ]]; then
   REPO=git@github.com:DockerConfLondon/DockerConfLondon.github.io.git
-  BRANCH=master
 else
-  REPO=git@github.com:DockerConfLondon/staging.git
-  BRANCH=gh-pages
+  REPO=git@github.com:DConfLdnStaging/DConfLdnStaging.git
 fi
 
 echo "DEPLOYING TO $REPO"
@@ -24,4 +22,4 @@ git --git-dir /tmp/jekyll-build/.git init .
 cd /tmp/jekyll-build/ && git add .
 git --git-dir /tmp/jekyll-build/.git commit -m "deploying"
 git --git-dir /tmp/jekyll-build/.git remote add origin $REPO
-git --git-dir /tmp/jekyll-build/.git push --force origin master:$BRANCH
+git --git-dir /tmp/jekyll-build/.git push --force origin master
