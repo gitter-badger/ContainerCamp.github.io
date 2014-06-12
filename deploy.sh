fe90a670557e844f27aeb3b425c3dfc7d2cf4382
Tim Ruffles <oi@truffles.me.uk>
2014-06-12 16:57:10 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+echo ${DOCKER_CON_ENV:-STAGING}
+
+if [[ ${DOCKER_CON_ENV:-STAGING} == "PRODUCTION" ]]; then
+  REPO=git@github.com:DockerConfLondon/DockerConfLondon.github.io.git
+  BRANCH=master
+else
+  REPO=git@github.com:DockerConfLondon/staging.git
+  BRANCH=gh-pages
+fi
+
+echo "DEPLOYING TO $REPO"
+
 echo "STASHING.."
 git stash
 
@@ -10,5 +23,5 @@ echo "DEPLOYING..."
 git --git-dir /tmp/jekyll-build/.git init .
 cd /tmp/jekyll-build/ && git add .
 git --git-dir /tmp/jekyll-build/.git commit -m "deploying"
-git --git-dir /tmp/jekyll-build/.git remote add origin git@github.com:DockerConfLondon/DockerConfLondon.github.io.git
-git --git-dir /tmp/jekyll-build/.git push --force origin master
+git --git-dir /tmp/jekyll-build/.git remote add origin $REPO
+git --git-dir /tmp/jekyll-build/.git push --force origin master:$BRANCH
