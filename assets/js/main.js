9fca8120d5d5ed9a7a89463b9ebf895b0a26a7ec
Chris Waring <chris@wwaves.co>
2015-01-24 00:04:20 +0100

    shipping

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 ;(function(){
-  $('#h .subscribe-input').focus();
+  if ($(window).width() > 760) $('#h .subscribe-input').focus();
   // open external links externally
   $('body').on('click', 'a[href^="//"], a[href^="http"], a[href^="mailto"]', function() {
     window.open($(this).attr('href'));
